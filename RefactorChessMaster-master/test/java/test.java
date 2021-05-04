55e8842ca33f387049e29d9766546730190ae2af
DeAngelo Wilson <deangelo76er@sbcglobal.net>
2021-05-04 16:49:37 -0500

    start test example

--- RefactorChessMaster-master/test/java/test.java
+++ RefactorChessMaster-master/test/java/test.java
@@ -1,2 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class test {
+
+
+
+    @Test
+    public void testMethodOne()
+    {
+        assertEquals(0,0);
+    }
 }
